7076c77bcc713d24c71d8b3aa25db2cac9cb5b8f
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-11-01 14:03:38 -0400

    remove flags

--- cmd/vars.go
+++ cmd/vars.go
@@ -6,10 +6,7 @@ Global variables live here
 
 */
 
-var cfgFile    string
-var domain     string
-var vaulthost  string
-var vaultport  string
+
 var tlsenable  bool
 var skipverify bool
 var verbose    bool
