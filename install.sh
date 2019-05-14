ec544a0b8c34fdf5cedc139c14c284e14e4e7e41
Joseph Sirianni <joe.sirianni@bluemedora.com>
2019-05-14 15:17:13 -0400

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd /usr/local/bin
-wget https://github.com/BlueMedoraPublic/bmcert/releases/download/0.2.0/bmcert-darwin-amd64.zip
-unzip -o bmcert-darwin-amd64.zip
+wget https://github.com/BlueMedoraPublic/bmcert/releases/download/v0.2.0/bmcert-v0.2.0-darwin-amd64.zip
+unzip -o bmcert-v0.2.0-darwin-amd64.zip
 chmod +x bmcert
-rm -f bmcert-darwin-amd64.zip
+rm -f bmcert-v0.2.0-darwin-amd64.zip
 which bmcert
 bmcert version
