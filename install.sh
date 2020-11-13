b1cf1ac392fa0333db51f572ea48a4c2098a97a3
Joseph Sirianni <joe.sirianni@bluemedora.com>
2020-11-12 21:50:11 -0500

    detect os. also handle goreleaser type releases, where additional files are included in the zip that we want to discard (avoid putting them in /usr/local/bin

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
-TAG=v1.1.1
-cd /usr/local/bin
-wget https://github.com/BlueMedoraPublic/bmcert/releases/download/$TAG/bmcert-$TAG-darwin-amd64.zip
-unzip -o bmcert-$TAG-darwin-amd64.zip
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     os=linux;;
+    Darwin*)    os=darwin;;
+    *)          os="UNKNOWN:${unameOut}"
+esac
+
+TAG=v1.1.2
+ZIP_FILE="bmcert_${TAG}_${os}_amd64.zip"
+wget "https://github.com/BlueMedoraPublic/bmcert/releases/download/${TAG}/${ZIP_FILE}"
+unzip -o -j "${ZIP_FILE}" "bmcert" -d "/usr/local/bin"
 chmod +x bmcert
-rm -f bmcert-$TAG-darwin-amd64.zip
+rm -f "${ZIP_FILE}"
 which bmcert
 bmcert version
