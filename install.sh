86c874eae54cdd6152791515e2e34fd090b24f1e
jsirianni <joe.sirianni@bluemedora.com>
2021-07-15 14:35:08 -0400

    install v1.1.5

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ case "${unameOut}" in
     *)          os="UNKNOWN:${unameOut}"
 esac
 
-TAG=v1.1.4
+TAG=v1.1.5
 ZIP_FILE="bmcert_${TAG}_${os}_amd64.zip"
 wget "https://github.com/BlueMedoraPublic/bmcert/releases/download/${TAG}/${ZIP_FILE}"
 unzip -o -j "${ZIP_FILE}" "bmcert" -d "/usr/local/bin"
